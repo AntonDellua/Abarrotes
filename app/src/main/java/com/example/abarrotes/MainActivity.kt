8d3804b997788b24d2a637236cd1d92a61d9a673
Diego <diegoramirezbarba@gmail.com>
2019-11-19 12:24:01 -0600

    Fix google maps dependencies

--- app/src/main/java/com/example/abarrotes/MainActivity.kt
+++ app/src/main/java/com/example/abarrotes/MainActivity.kt
@@ -3,44 +3,19 @@ package com.example.abarrotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import org.jetbrains.anko.find
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback, BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     // Fragment Declarations
-    private val homeFragment /*: Fragment*/ = null
+    private val homeFragment = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         // Normal beginning, set content to main layout
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Instance Google Maps
-        val mMapFragment = SupportMapFragment.newInstance()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.map, mMapFragment)
-            .commit()
-
-        mMapFragment.getMapAsync(this)
     }
-
-    // Initialize Google Maps
-    override fun onMapReady(map: GoogleMap?) {
-        map?.addMarker(
-            MarkerOptions()
-            .position(LatLng(0.0, 0.0))
-            .title("Marker"))
-    }
-
 
     // Nav Menu Functionality
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
