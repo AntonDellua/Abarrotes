ef460f51368761b6699254daa9c7d4a07dfd1e64
AntonDellua <antondellua@gmail.com>
2019-11-27 18:15:22 -0600

    Added Push Notifications

--- app/src/main/java/com/example/abarrotes/AbarrotesApplication.kt
+++ app/src/main/java/com/example/abarrotes/AbarrotesApplication.kt
@@ -2,15 +2,25 @@ package com.example.abarrotes
 
 import android.app.Application
 import com.parse.Parse
+//import javax.swing.UIManager.put
+import com.parse.ParseInstallation
+import com.example.abarrotes.R.*
 
 class AbarrotesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Parse.initialize(
-            Parse.Configuration.Builder(this).applicationId("Kcsogp0q6eCS30a7Z84MXMzgtdcqoRPmNbnUmuvx")
-                .clientKey("y92AE7wlclY2OoJKZ2tMrcw4KQ8KD8YRfMNQMKNt")
-                .server("https://parseapi.back4app.com/")
+            Parse.Configuration.Builder(this)
+                .applicationId(getString(string.back4app_app_id))
+                // if defined
+                .clientKey(getString(string.back4app_client_key))
+                .server(getString(string.back4app_server_url))
                 .build()
         )
+        // Parse Push Notification
+        val installation = ParseInstallation.getCurrentInstallation()
+        installation.put("GCMSenderId", "602907222268")
+        installation.saveInBackground()
+
     }
 }
