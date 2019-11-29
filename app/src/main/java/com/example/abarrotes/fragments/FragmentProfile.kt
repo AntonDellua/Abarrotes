3214ea7a2ccf8aa838c48d51713fbfadad8e2288
Diego <diegoramirezbarba@gmail.com>
2019-11-29 01:53:59 -0600

    Add user name and email to profile

--- app/src/main/java/com/example/abarrotes/fragments/FragmentProfile.kt
+++ app/src/main/java/com/example/abarrotes/fragments/FragmentProfile.kt
@@ -6,13 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.os.Bundle
+import com.parse.ParseUser
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class FragmentProfile : Fragment() {
+    private var mUsername = ""
+    private var mEmail = ""
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        var view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val user = ParseUser.getCurrentUser()
+        mUsername = user.username
+        mEmail = user.email
+
+        view.name_text.text = mUsername
+        view.email_text.text = mEmail
+
+        return view
     }
 }
